2c31a601653417370a9c678044d3ddfe009c7e81
Aidan Jalali <aidanjalali@gmail.com>
2014-06-05 16:30:00 +0100

    attempting to style pagination

--- www/app/rainbowlemon.js
+++ www/app/rainbowlemon.js
@@ -41,6 +41,8 @@ define([
             },
             
             greeting: $('#intro-greeting'),
+            portfolioContainer: $('#portfolio-container'),
+            portfolioOverview: $('#portfolio-overview'),
             portfolio: $('#portfolio'),
             entries: $('#portfolio > section')
         },
@@ -49,7 +51,7 @@ define([
             $(window).on('scroll.' + this.options.appName, this.setColors.bind(this));
             $(window).on('hashchange.' + this.options.appName, this.changePage.bind(this));
             
-            this.setColors(); //set page colors before scroll
+            this.setColors();
         },
         
         setColors: function() { 
@@ -76,13 +78,44 @@ define([
         
         setupPage: function(){
             this.el.greeting.text(_.sample(this.options.greetings)).show();
+            this.changePage();
         },
         
         changePage: function(e){
-            console.log(e);
+            var loc = (window.location.hash.indexOf('/') == -1) ? '' : window.location.hash.split('/')[1];
             
-            if (e.hash === '#'){
-                console.log('home');
+            if (loc !== ''){
+                var $active = this.el.portfolio.find('[data-entry="'+ loc + '"]'),
+                    $img = $active.find('img[data-src]');
+                
+                //Hide any portfolio entries that might already be shown
+                this.el.portfolio.find('section:visible').hide();
+                
+                $img.each(function(){
+                    var $this = $(this);
+                    
+                    $this.attr('src', $this.data('src'));
+                });
+                
+                //Need to set the height on the container so the overview doesn't disappear when setting to position:absolute
+                /*
+                this.el.portfolioContainer.css({
+                    'height': this.el.portfolioOverview.outerHeight(true)
+                });
+                */
+                this.el.portfolioOverview.css('position', 'absolute');
+                
+                $active.show();
+                this.el.portfolioContainer.attr('data-page', '2');
+            } else {
+                //Set the height on the portfolio container again, in case it has changed
+                /*
+                this.el.portfolioContainer.css({
+                    'height': this.el.portfolioOverview.outerHeight(true)
+                });
+                */
+                
+                this.el.portfolioContainer.attr('data-page', '1');
             }
         }
     
